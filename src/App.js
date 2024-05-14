8b1b767281f50912d61d43c9c23b1e95e785531f
jayardeshana <jay.a@brilworkss.com>
2024-05-14 12:09:07 +0530

    Add Itemlist

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Counter from "./component/Counter";
+import ListOfItems from "./component/ListOfItems";
 
 function App() {
   return (
-    <div className="App">
-      <Counter />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/items" element={<ListOfItems />} />
+        <Route path="/" element={<Counter />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
